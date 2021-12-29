181e62f96eb71749bdfa7828eefa53e533c4e859
neonilakunyk <n.kunyk-2025@kpi.ua>
2021-12-29 14:55:51 +0200

    Fixed config

--- env-config.js
+++ env-config.js
@@ -3,14 +3,12 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 export const envConfig = {
-  app: {
-    port: process.env.PORT,
-    host: process.env.HOST
-  },
   db: {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     name: process.env.DB_NAME,
     cluster: process.env.DB_CLUSTER
-  }
+  },
+  port: process.env.PORT,
+  host: process.env.HOST
 };
--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 import fastify from 'fastify';
 import fastifyCors from 'fastify-cors';
+import { routes } from './src/routes';
+import { envConfig } from './env-config';
 import { connectDB } from './src/data/db';
 import { logger } from './src/utils';
-import { envConfig } from './env-config';
-import { routes } from './src/routes';
 
 const server = fastify();
 
 server.register(fastifyCors, {
-  origin: ['http://localhost:3001/', 'https://zalic.herokuapp.com/'],
+  origin: ['http://0.0.0.0:3001', 'https://zalic.herokuapp.com/'],
   methods: ['GET', 'POST']
 });
 
@@ -18,14 +18,10 @@ routes.forEach((route) => {
 
 connectDB();
 
-const {
-  app: { port, host }
-} = envConfig;
-
-server.listen(port || 3000, host, (err) => {
+server.listen(envConfig.port || 3000, envConfig.host, (err) => {
   if (err) {
     logger.error(err);
     process.exit(1);
   }
-  logger.info(`Server listening at port ${port}`);
+  logger.info(`Server listening at port ${envConfig.port}`);
 });
